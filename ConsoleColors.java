66790c93f1cabbde6beedf83c90210422f02f1f2
devini15 <devini554@gmail.com>
2024-07-02 10:38:34 -0500

    Create ConsoleColors.java

---
+++ ConsoleColors.java
@@ -0,0 +1,8 @@
+public class ConsoleColors {
+    public static final String RESET = "\u001b[0m";
+    public static final String RED = "\u001b[0;31m";
+    public static final String GREEN = "\u001b[0;32m";
+    public static final String YELLOW = "\u001b[0;33m";
+    public static final String BLUE = "\u001b[0;34m";
+    public static final String CYAN = "\u001b[0;36m";
+}
